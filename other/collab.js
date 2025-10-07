48604257967b52f1ea1ee31a3b5a376ba0a4fdb2
Camila Prá <camila@guiprav.com>
2025-10-07 17:03:59 -0300

    .

--- other/collab.js
+++ other/collab.js
@@ -1,13 +1,9 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js';
 import EventEmitter from 'https://esm.sh/eventemitter2';
+import { createClient } from 'https://esm.sh/@supabase/supabase-js';
 
-// FIXME: Rewrite
 class RealtimeCollaboration {
   constructor(room) {
-    this.supabase = createClient(
-      'https://xvxszwvhwzsxjhxskkek.supabase.co',
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh2eHN6d3Zod3pzeGpoeHNra2VrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgyODY2MjcsImV4cCI6MjA3Mzg2MjYyN30.tQi5XePGi6NHLAfuKEuMSw7YQCiDVY2KAd8XXmk8Lcs',
-    );
+    this.supabase = createClient('https://xvxszwvhwzsxjhxskkek.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh2eHN6d3Zod3pzeGpoeHNra2VrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgyODY2MjcsImV4cCI6MjA3Mzg2MjYyN30.tQi5XePGi6NHLAfuKEuMSw7YQCiDVY2KAd8XXmk8Lcs');
     this.channel = null;
     this.events = new EventEmitter({ wildcard: true, delimiter: ':' });
     this.presence = [];
@@ -15,42 +11,7 @@ class RealtimeCollaboration {
   }
 
   async joinRoom(room) {
-    let colors = [
-      'red-600',
-      'red-800',
-      'orange-600',
-      'orange-800',
-      'amber-600',
-      'amber-800',
-      'yellow-600',
-      'yellow-800',
-      'lime-600',
-      'lime-800',
-      'green-600',
-      'green-800',
-      'emerald-600',
-      'emerald-800',
-      'teal-600',
-      'teal-800',
-      'cyan-600',
-      'cyan-800',
-      'sky-600',
-      'sky-800',
-      'blue-600',
-      'blue-800',
-      'indigo-600',
-      'indigo-800',
-      'violet-600',
-      'violet-800',
-      'purple-600',
-      'purple-800',
-      'fuchsia-600',
-      'fuchsia-800',
-      'pink-600',
-      'pink-800',
-      'rose-600',
-      'rose-800',
-    ];
+    let colors = ['red-600', 'red-800', 'orange-600', 'orange-800', 'amber-600', 'amber-800', 'yellow-600', 'yellow-800', 'lime-600', 'lime-800', 'green-600', 'green-800', 'emerald-600', 'emerald-800', 'teal-600', 'teal-800', 'cyan-600', 'cyan-800', 'sky-600', 'sky-800', 'blue-600', 'blue-800', 'indigo-600', 'indigo-800', 'violet-600', 'violet-800', 'purple-600', 'purple-800', 'fuchsia-600', 'fuchsia-800', 'pink-600', 'pink-800', 'rose-600', 'rose-800'];
 
     this.uid = 'master';
     this.color = colors[Math.floor(Math.random() * colors.length)];
